517b0d37198c312de9a473234d89768cdf643c61
4B6555S <monga.wakowa@gmail.com>
2021-03-15 16:12:05 +0200

    Update live.py

--- live/live.py
+++ live/live.py
@@ -7,7 +7,7 @@ def Onstart():
     b = 0
     while 1:
         print(b)
-        time.sleep(1)
+        time.sleep(5)
         random.random()
         b = random.randint(0, 50)
         os.system("/usr/bin/mosquitto_pub -h 127.0.0.1 -t ch1/temp -m " + str(b))
